65e7b1eb6d493411dec8699ddcb4226275ef2d91
Sheprut Ilya <optitel223@gmail.com>
2017-10-21 01:55:31 +0700

    Make transformations

--- include/pt/transformation.h
+++ include/pt/transformation.h
@@ -7,17 +7,16 @@ namespace pt
 {
 
 	class Transformation;
-	/*class TrMove;
+	class TrMove;
 	class TrRotate;
 	class TrScale;
-	class TrLookAt;*/
+	/*class TrLookAt;*/
 
 	//-------------------------------------------------------------------------
 	class Transformation : public Object
 	{
 	public:
 		Transformation(Object* obj) : m_obj(obj) {}
-		virtual ~Transformation() {}
 
 		bool intersect(const Ray& ray, 
 					   Intersection& inter, 
@@ -29,12 +28,69 @@ namespace pt
 							Ray& scattered,
 							double& diffusion) const;
 
-	protected:
-		virtual Ray transform(const Ray& ray) const = 0;
-		virtual Ray inverse(const Ray& ray) const = 0;
+		virtual Vector transform(const Vector& p) const = 0;
+		virtual Vector inverse(const Vector& p) const = 0;
 		
 	private:
 		Object* m_obj;
+		mutable Ray ray1;
+		mutable Intersection inter1;
+	};
+
+	//-------------------------------------------------------------------------
+	class TrMove : public Transformation
+	{
+	public:
+		TrMove(Vector diff, Object* obj);
+
+		Vector transform(const Vector& p) const;
+		Vector inverse(const Vector& p) const;
+
+		Vector diff;
+	};
+
+	//-------------------------------------------------------------------------
+	class TrRotate : public Transformation
+	{
+	public:
+		TrRotate(Vector angles, Object* obj);
+
+		Vector transform(const Vector& p) const;
+		Vector inverse(const Vector& p) const;
+
+		void rotate2(double& x, double &y, const double& angle) const;
+
+		Vector angles; 
+	};
+
+	//-------------------------------------------------------------------------
+	class TrScale : public Transformation
+	{
+	public:
+		TrScale(Vector scale, Object* obj);
+
+		Vector transform(const Vector& p) const;
+		Vector inverse(const Vector& p) const;
+
+		Vector scale;
+	};
+
+	//-------------------------------------------------------------------------
+	class TrComplex : public Transformation
+	{
+	public:
+		TrComplex(Vector scale, Vector angles, Vector diff, Object* obj);
+
+		Vector transform(const Vector& p) const;
+		Vector inverse(const Vector& p) const;
+
+		Vector& scale;
+		Vector& angles;
+		Vector& diff;
+	private:
+		TrScale m_scale;
+		TrRotate m_rotate;
+		TrMove m_move;
 	};
 
 };
--- src/transformation.cpp
+++ src/transformation.cpp
@@ -8,8 +8,19 @@ bool Transformation::intersect(const Ray& ray,
 							   Intersection& inter, 
 							   double tMin, 
 							   double tMax) const {
-	// @TODO make this
-	return false;
+	ray1.pos = transform(ray.pos);
+	ray1.dir = transform(ray.pos + ray.dir) - ray1.pos;
+
+	bool returned = m_obj->intersect(ray1, inter1, tMin, tMax);
+
+	if (returned) {
+		inter.t = inter1.t;
+		inter.pos = inverse(inter1.pos);
+		inter.normal = inverse(inter1.pos + inter1.normal) - inter.pos;
+		inter.normal.normalize();
+		return true;
+	} else
+		return false;
 }
 
 //-----------------------------------------------------------------------------
@@ -18,8 +29,101 @@ ScatterType Transformation::scatter(const Ray& ray,
 									Color& clrAbsorbtion,
 									Ray& scattered,
 									double& diffusion) const {
-	// @TODO make this
-	return SCATTER_END;
+	//Ray ray1;
+	//Intersection inter1;
+	Ray scattered1;
+
+	//ray1.pos = transform(ray.pos);
+	//ray1.dir = transform(ray.pos + ray.dir) - ray1.pos;
+
+	//inter1.t = inter.t;
+	//inter1.pos = transform(inter.pos);
+	//inter1.normal = transform(inter.pos + inter.normal) - inter1.pos;
+
+	ScatterType returned = m_obj->scatter(ray1, inter1, clrAbsorbtion, scattered1, diffusion);
+
+	scattered.pos = inverse(scattered1.pos);
+	scattered.dir = inverse(scattered1.pos + scattered1.dir) - scattered.pos;
+	scattered.dir.normalize();
+	return returned;
+}
+
+//-----------------------------------------------------------------------------
+TrMove::TrMove(Vector diff, Object* obj) : diff(diff), Transformation(obj) {}
+
+//-----------------------------------------------------------------------------
+Vector TrMove::transform(const Vector& p) const {
+	return p - diff;
+}
+
+//-----------------------------------------------------------------------------
+Vector TrMove::inverse(const Vector& p) const {
+	return p + diff;
+}
+
+//-----------------------------------------------------------------------------
+TrRotate::TrRotate(Vector angles, Object* obj) : angles(angles), Transformation(obj) {}
+
+//-----------------------------------------------------------------------------
+void TrRotate::rotate2(double& x, double &y, const double& angle) const {
+	double x1 = cos(angle)*x - sin(angle)*y;
+	double y1 = sin(angle)*x + cos(angle)*y;
+	x = x1;
+	y = y1;
+}
+
+//-----------------------------------------------------------------------------
+Vector TrRotate::transform(const Vector& p) const {
+	Vector a = p;
+	rotate2(a.x, a.y, angles.z);
+	rotate2(a.x, a.z, angles.y);
+	rotate2(a.y, a.z, angles.x);
+	return a;
+}
+
+//-----------------------------------------------------------------------------
+Vector TrRotate::inverse(const Vector& p) const {
+	Vector a = p;
+	rotate2(a.y, a.z, -angles.x);
+	rotate2(a.x, a.z, -angles.y);
+	rotate2(a.x, a.y, -angles.z);
+	return a;
+}
+
+//-----------------------------------------------------------------------------
+TrScale::TrScale(Vector scale, Object* obj) : scale(scale), Transformation(obj) {}
+
+//-----------------------------------------------------------------------------
+Vector TrScale::transform(const Vector& p) const {
+	Vector a = p;
+	a.x /= scale.x;
+	a.y /= scale.y;
+	a.z /= scale.z;
+	return a;
+}
+
+//-----------------------------------------------------------------------------
+Vector TrScale::inverse(const Vector& p) const {
+	Vector a = p;
+	a.x *= scale.x;
+	a.y *= scale.y;
+	a.z *= scale.z;
+	return a;
+}
+
+//-----------------------------------------------------------------------------
+TrComplex::TrComplex(Vector scale, Vector angles, Vector diff, Object* obj) : m_scale(scale, nullptr), m_rotate(angles, nullptr), m_move(diff, nullptr), scale(m_scale.scale), angles(m_rotate.angles), diff(m_move.diff), Transformation(obj) {}
+
+//-----------------------------------------------------------------------------
+Vector TrComplex::transform(const Vector& p) const {
+	//return m_move.transform(m_rotate.transform(m_scale.transform(p)));
+	return m_scale.transform(m_rotate.transform(m_move.transform(p)));
+}
+
+//-----------------------------------------------------------------------------
+Vector TrComplex::inverse(const Vector& p) const {
+	//return m_scale.inverse(m_rotate.inverse(m_move.inverse(p)));
+	return m_move.inverse(m_rotate.inverse(m_scale.inverse(p)));
 }
 
 };
