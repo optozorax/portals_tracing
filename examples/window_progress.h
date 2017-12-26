0e353b8770776b7401766cc7127a35ac7dbb479b
Sheprut Ilya <optitel223@gmail.com>
2017-12-27 00:39:39 +0700

    Add window with progress of rendering

---
+++ examples/window_progress.h
@@ -0,0 +1,40 @@
+#ifndef PT_WINDOW_PROGRESS_H
+#define PT_WINDOW_PROGRESS_H
+
+#include <twg/twg.h>
+#include <twg/window/window_events.h>
+
+#include <pt/pt.h>
+#include <pt/pt2twg.h>
+
+#include <pt/renderer/thread-renderer.h>
+
+namespace pt
+{
+
+	/** Тип окна, который показывает прогресс рендеринга изображения с любым типом рендеринга, любой камерой, любой сценой. Для этого всего необходимо передать окну размеры изображения, указатель на камеру, указатель на сцену, имя файла куда сохранить после рендеринга. После рендеринга окно закроется. */
+	class WindowProgress : public twg::WindowEvents
+	{
+	public:
+		WindowProgress(twg::Point_i size, Renderer* ren, Camera* cam, Object* scene, std::string imageName);
+		~WindowProgress();
+
+		//---------------------------------------------------------------------
+		bool onMessage(twg::int32u id, void* data);
+	private:
+		Image					img;
+		ThreadPartiallyRender	ren;
+		Camera*					cam;
+		Object*					scene;
+
+		std::string				imageName;
+
+		bool					isStarted;
+		bool					sizeInstalled;
+
+		long					time;
+	};
+
+};
+
+#endif
