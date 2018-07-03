b51e1c619f36bf8fffc47cc0018bcbe757593a84
Ilya Sheprut <optitel223@gmail.com>
2018-07-03 23:22:49 +0600

    Sphere bug fixed
    Fixed bug when points on sphere are black

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -15,22 +15,22 @@ bool Sphere::intersect(const Ray& ray,
 	double pp = dot(P, P);
 	double pv = dot(P, V);
 
-	float a = vv;
-	float b = 2.0 * pv;
-	float c = pp - r*r;
+	double a = vv;
+	double b = 2.0 * pv;
+	double c = pp - r*r;
 
-	float d = b*b - 4*a*c;
+	double d = b*b - 4.0*a*c;
 	if (d >= 0) {
 		d = sqrt(d);
-		float t1 = (-b-d)/(2.0*a);
-		float t2 = (-b+d)/(2.0*a);
+		double t1 = (-b-d)/(2.0*a);
+		double t2 = (-b+d)/(2.0*a);
 		if (t1 > tMin && t1 < tMax) {
 			inter.t = t1;
 			inter.pos = ray.pos + ray.dir * t1;
 			inter.normal = inter.pos - A;
 			inter.normal.normalize();
-			if (dot(ray.dir, inter.normal) >= 0)
-				inter.normal = -inter.normal;
+			//if (dot(ray.dir, inter.normal) >= 0)
+				//inter.normal = -inter.normal;
 			return true;
 		} else
 		if (t2 > tMin && t2 < tMax) {
@@ -38,8 +38,8 @@ bool Sphere::intersect(const Ray& ray,
 			inter.pos = ray.pos + ray.dir * t2;
 			inter.normal = inter.pos - A;
 			inter.normal.normalize();
-			if (dot(ray.dir, inter.normal) >= 0)
-				inter.normal = -inter.normal;
+			//if (dot(ray.dir, inter.normal) >= 0)
+				//inter.normal = -inter.normal;
 			return true;
 		} else
 			return false;
